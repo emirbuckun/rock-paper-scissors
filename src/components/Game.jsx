01ecb12fcec64b2e61826cd9195651e7c38ae434
Emir Buckun <emirbuckun@hotmail.com>
2023-03-22 22:00:09 +0300

    Game component style improved. Box-shadow for winner added. Icon rendering made shorter.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -46,22 +46,18 @@ const Game = ({ userChoice, score, setScore }) => {
     <div className="game">
       <div className="game-user">
         <span className="game-text"> You Picked</span>
-        {userChoice == "paper" && (
-          <div className="game-icon game-icon-paper"></div>
-        )}
-        {userChoice == "rock" && (
-          <div className="game-icon game-icon-rock"></div>
-        )}
-        {userChoice == "scissors" && (
-          <div className="game-icon game-icon-scissors"></div>
-        )}
+        <div
+          className={
+            "game-icon game-icon-" +
+            userChoice +
+            (playerWin === "win" ? " icon-winner-" + userChoice : "")
+          }
+        ></div>
       </div>
 
       <div className="game-play">
         <span className="game-play-text">
-          {playerWin == "win" && "You Win"}
-          {playerWin == "lose" && "You Lose"}
-          {playerWin == "draw" && "Draw"}
+          {playerWin === "draw" ? playerWin : "you " + playerWin}
         </span>
         <Link to="/" className="play-again" onClick={() => setComputer()}>
           Play Again
@@ -70,26 +66,16 @@ const Game = ({ userChoice, score, setScore }) => {
 
       <div className="game-computer">
         <span className="game-text">Computer Picked</span>
-        {computer == "paper" && (
-          <div className="game-icon game-icon-paper"></div>
-        )}
-        {computer == "rock" && <div className="game-icon game-icon-rock"></div>}
-        {computer == "scissors" && (
-          <div className="game-icon game-icon-scissors"></div>
-        )}
+        <div
+          className={
+            "game-icon game-icon-" +
+            computer +
+            (playerWin === "lose" ? " icon-winner-" + computer : "")
+          }
+        ></div>
       </div>
     </div>
   );
 };
 
 export default Game;
-
-// User Choice: {userChoice} <br />
-//       Computer Choice: {computer} <br />
-//       Result:
-//       {playerWin === "win" && <h2>You Win</h2>}
-//       {playerWin === "lose" && <h2>You Lose</h2>}
-//       {playerWin === "draw" && <h2>Draw</h2>}
-//       <Link to="/" onClick={() => setComputer()}>
-//         Play Again
-//       </Link>
