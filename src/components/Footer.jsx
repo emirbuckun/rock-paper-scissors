f6aecea08951b763e0d1d8c83537419cc713ad35
Emir Buckun <emirbuckun@hotmail.com>
2023-03-23 09:23:13 +0300

    Style footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div class="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Emir Büçkün</a>.
+      </div>
+      <button className="rules">Rules</button>
+    </footer>
+  );
 };
 
 export default Footer;
