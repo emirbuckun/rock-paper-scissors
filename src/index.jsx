5eac548c260b8c5f57b46ba647670b520021427f
Emir Buckun <emirbuckun@hotmail.com>
2023-03-26 15:52:42 +0300

    Remove homepage

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "/src/App";
 import "/src/index.css";
-import { createHashRouter, RouterProvider } from "react-router-dom";
 
 const router = createHashRouter([
   {
